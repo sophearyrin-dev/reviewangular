6097dfb3720eaf3212611e04801a4a47ee2e095e
sophearyrin1 <srin@miu.edu>
2025-02-14 04:28:32 -0800

    component communication

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, input } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.css'
+})
+export class ChildComponent {
+
+  @Input() message : string = "";
+
+}
