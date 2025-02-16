624d79b46755656fdf82c4a796099ae8c43e9c94
sophearyrin1 <srin@miu.edu>
2025-02-15 22:11:38 -0800

    @output

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, EventEmitter, Input, input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -8,5 +8,18 @@ import { Component, Input, input } from '@angular/core';
 export class ChildComponent {
 
   @Input() message : string = "";
+  @Input() message2 : string = "";
+  @Output() messageEvent = new EventEmitter<string>();
+  @Output() messageEvent2 = new EventEmitter<string>();
+
+  onSendMessageToParent(){
+    this.messageEvent.emit("Hello Parents, I hope you are doing good");
+  }
+
+  onSeekingMoney(){
+    this.messageEvent2.emit("Papa, Can I have some money?");
+  }
+
+  
 
 }
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
 })
 export class ParentComponent {
 
-  messageSendToChild: string = "Hello Child! From your Parent :)"
+  messageSendToChild: string = "Hello Child! From your Parent :)";
+  messageGoodByeChild: string = "Good bye my child!"
+
+  msg: string = "";
+  msg2: string = "";
+
+  receivedMessageFromChild(msg:string){
+    this.msg = msg;
+  }
+
+  receiveMsg2FromChild(msg2: string){
+    this.msg2 = msg2;
+  }
 
 }
